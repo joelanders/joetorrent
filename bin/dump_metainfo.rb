bf5528913c571470c14e389f34fb8f2e6ce3aa04
Joe Landers <joe@joelanders.net>
2013-10-19 22:54:42 -0700

    can get a connection response from udp tracker

--- bin/dump_metainfo.rb
+++ bin/dump_metainfo.rb
@@ -2,10 +2,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib',__FILE__)
 require 'joetorrent'
 require 'digest/sha1'
+require 'pry'
 
 mi = Metainfo.from_file ARGV[0]
 #p mi.hash
 p mi.info_hash_hex
 p mi.tracker_uri
-p mi.tracker.announce
-p Hash.from_bencoding mi.tracker.last_response.body.to_s
+#p mi.tracker.announce
+#p Hash.from_bencoding mi.tracker.last_response.body.to_s
+binding.pry
--- lib/joetorrent/tracker.rb
+++ lib/joetorrent/tracker.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'socket'
+require 'resolv'
 
 class Tracker
   attr_accessor :last_response
@@ -7,6 +9,40 @@ class Tracker
     @last_response = nil
   end
 
+  def announce
+    @last_response = UdpTrackerRequest.
+      new( URI(@metainfo.tracker_uri) ).
+      send
+  end
+end
+
+class UdpTrackerRequest
+  CON_REQ_ID  = "\x00\x00\x04\x17\x27\x10\x19\x80"
+  CON_REQ_ACT = "\x00\x00\x00\x00"
+  def initialize(uri)
+    @uri = uri
+    @last_con_resp = nil
+    r = Resolv::DNS.open({:nameserver=>['208.67.222.222']}) #really?
+    @ip = r.getaddress(@uri.host)
+  end
+
+  def send
+    usock = UDPSocket.new
+    usock.bind('', 6882)
+    puts @ip.to_s
+    puts @uri.port
+    p con_req
+    usock.connect @ip.to_s, @uri.port
+    usock.send con_req, 0
+    @last_con_resp = usock.recvfrom(20,0)
+  end
+
+  def con_req
+    CON_REQ_ID + CON_REQ_ACT + Random.new.bytes(4)
+  end
+end
+
+class HttpTrackerRequest
   def announce
     params = { :info_hash  => @metainfo.info_hash,
                :peer_id    => '0123456789testing123',
