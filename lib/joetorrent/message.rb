b6a9fa0870ad61f53082a58bf7cb38e62fcac805
Joe Landers <joe@joelanders.net>
2013-10-26 14:59:43 -0700

    peer to peer message format

---
+++ lib/joetorrent/message.rb
@@ -0,0 +1,81 @@
+class Message
+  def self.keep_alive
+    [0].pack 'L>'
+  end
+
+  def self.choke
+    [1].pack('L>') + "\x00"
+  end
+
+  def self.unchoke
+    [1].pack('L>') + "\x01"
+  end
+
+  def self.interested
+    [1].pack('L>') + "\x02"
+  end
+
+  def self.not_interested
+    [1].pack('L>') + "\x03"
+  end
+
+  def self.have piece_index
+    [5].pack('L>') + "\x04" +
+      [piece_index].pack('L>')
+  end
+
+  # num_pieces is total number of pieces in the torrent
+  # nasty as hell; find a better way sometime.
+  def self.bitfield piece_indices, num_pieces
+    bigint = 0
+    piece_indices.each do |piece_index|
+      bigint |= (1<<piece_index)
+    end
+
+    num_bytes = (num_pieces.to_f / 8).ceil
+    bitfield = ""
+
+    num_bytes.times do
+      byte = bigint % (1<<8)
+      rev_byte = 0
+      8.times do # reverse bit-order in the byte...
+        rev_byte = rev_byte << 1
+        rev_byte += byte % 2
+        byte = byte >> 1
+      end
+      bitfield += [rev_byte].pack 'C'
+      bigint = bigint >> 8
+    end
+
+    [1+bitfield.length].pack('L>') + "\x05" + bitfield
+  end
+
+  # offset is the starting position of the requested block
+  # within the piece.
+  def self.request piece_index, offset, block_length
+    [13].pack('L>') + "\x06" +
+      [piece_index].pack('L>') +
+      [offset].pack('L>') +
+      [block_length].pack('L>')
+  end
+
+  # not a metainfo "piece," this is actually a "block"
+  def self.piece piece_index, offset, block_data
+    [9+block_data.length].pack('L>') + "\x07" +
+      [piece_index].pack('L>') +
+      [offset].pack('L>') +
+      block_data
+  end
+
+  def self.cancel piece_index, offset, block_length
+    [13].pack('L>') + "\x08" +
+      [piece_index].pack('L>') +
+      [offset].pack('L>') +
+      [block_length].pack('L>')
+  end
+
+  # for DHT
+  def self.port number
+    [3].pack('L>') + "\x09" + [number].pack('S>')
+  end
+end
---
+++ spec/joetorrent/message_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+# todo: make Messages throw exceptions on invalid parameters.
+describe Message do
+  describe ".keep_alive" do
+    it "is always the same message" do
+      Message.keep_alive.should eq "\x00\x00\x00\x00"
+    end
+  end
+
+  describe ".choke" do
+    it "is always the same message" do
+      Message.choke.should eq "\x00\x00\x00\x01\x00"
+    end
+  end
+
+  describe ".unchoke" do
+    it "is always the same message" do
+      Message.unchoke.should eq "\x00\x00\x00\x01\x01"
+    end
+  end
+
+  describe ".interested" do
+    it "is always the same message" do
+      Message.interested.should eq "\x00\x00\x00\x01\x02"
+    end
+  end
+
+  describe ".not_interested" do
+    it "is always the same message" do
+      Message.not_interested.should eq "\x00\x00\x00\x01\x03"
+    end
+  end
+
+  describe ".have" do
+    it "encodes a 32-bit piece index" do
+      Message.have(  0).should eq "\x00\x00\x00\x05\x04\x00\x00\x00\x00"
+      Message.have(256).should eq "\x00\x00\x00\x05\x04\x00\x00\x01\x00"
+    end
+  end
+
+  describe ".bitfield" do
+    it "single piece bitfield" do
+      Message.bitfield( [0], 1 ).should eq( [2].pack('L>') + "\x05\x80" )
+      Message.bitfield( [], 1 ).should eq( [2].pack('L>') + "\x05\x00" )
+    end
+    it "single byte (8 pieces) bitfield" do
+      Message.bitfield( [0, 3, 2, 4], 8 ).
+        should eq( [2].pack('L>') + "\x05\xb8" ) # 0b10111000 = 0xb8
+    end
+    it "two byte (12 pieces) bitfield" do
+      Message.bitfield( [0, 10], 12 ).
+        should eq( [3].pack('L>') + "\x05\x80\x20" ) # 0b10111000 = 0xb8
+    end
+    it "12 byte (90 pieces) bitfield" do
+      Message.bitfield( [1, 15, 88, 89], 90 ).
+        should eq( [13].pack('L>') +
+                   "\x05\x40\x01" + "\x00"*9 + "\xc0" )
+    end
+  end
+
+  # identical to .cancel except for message id
+  describe ".request" do
+    it "encodes piece_index, offset, block_length" do
+      Message.request( 257, 2**16, 2**14 ).
+        should eq [13].pack('L>') + "\x06" +
+                  [257].pack('L>') +
+                  [2**16].pack('L>') +
+                  [2**14].pack('L>')
+    end
+  end
+
+  describe ".piece" do
+    it "encodes piece_index, offset, and block_data" do
+      Message.piece( 2, 0, "\xff\xfb\x90\x44" ).
+        should eq [13].pack('L>') + "\x07" +
+                  [2].pack('L>') +
+                  [0].pack('L>') +
+                  "\xff\xfb\x90\x44"
+    end
+  end
+
+  # identical to .request except for message id
+  describe ".cancel" do
+    it "encodes piece_index, offset, block_length" do
+      Message.cancel( 257, 2**16, 2**14 ).
+        should eq [13].pack('L>') + "\x08" +
+                  [257].pack('L>') +
+                  [2**16].pack('L>') +
+                  [2**14].pack('L>')
+    end
+  end
+end
